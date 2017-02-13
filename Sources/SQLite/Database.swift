fe11761bba0ec37959d1b25c9fd28cc9231205a1
David Skrundz <david@skrundz.ca>
2017-02-13 12:15:51 -0700

    Add a public init to Database

--- Sources/SQLite/Database.swift
+++ Sources/SQLite/Database.swift
@@ -6,6 +6,10 @@
 public struct Database {
 	public let fileName: String
 	
+	public init(fileName: String) {
+		self.fileName = fileName
+	}
+	
 	public func connect() throws -> Connection {
 		return try Connection(fileName: self.fileName)
 	}
